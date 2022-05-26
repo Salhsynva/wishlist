a72ac580b76837a6a9e436b4508e956cbdedc9f2
Salhsynva <6mgc0gt@code.edu.az>
2022-05-26 14:10:35 +0400

    change

--- wishlist/assets/js/main.js
+++ wishlist/assets/js/main.js
@@ -1,37 +1,63 @@
-let wishlist;
+
+let wishCount = document.getElementById('wishCount');
 let page = 1;
-
-let wishlistStr = localStorage.getItem('wishlist');
-
-if (!wishlistStr) {
-    wishlist = [];
-} else {
-    wishlist = JSON.parse(wishlistStr);
+let list = document.getElementById('list')
+let search =document.getElementById('search')
+if(wishCount){
+    loadProducts(page)
 }
 
+let wishlist
+let wishlistStr = localStorage.getItem('wishlist');
 
-let badge = document.querySelector('#wishCount');
-badge = wishlist.length;
+if(wishlistStr)
+    wishlist = JSON.parse(wishlistStr);
+else
+    wishlist = []
+
+if(wishCount)
+    wishCount.innerText = wishlist.length;
 
 
-let loadBtn = document.querySelector('.btn');
+    let loadBtn = document.getElementById('loadBtn');
+    if(loadBtn){
+        document.getElementById('loadBtn').addEventListener('click', function (e) {
+            page++;
+            loadProducts(page)
+        })
+    }
 
-loadBtn.addEventListener('click', function () {
 
-    LoadinPages();
-    page++;
+search.addEventListener('input' ,function(){
+    let oldli = document.querySelectorAll('#list li')
+    oldli.forEach(x=>{
+        x.remove();
+    })
 
+    fetch(`https://dummyjson.com/products/search?q=${search.value}`)
+        .then(response => response.json())
+        .then(data => {
+            data.products.forEach(y=>{
+                let li = ` <li class="list-group-item"> ${y.brand}--------------${y.title}/li>`
+                list.innerHTML+=li
+            })
+
+        })
 })
 
+search.addEventListener('blur' ,function(){
+  list.style.display = 'none'
+})
 
-function LoadinPages() {
+function loadProducts(page) {
     fetch(`https://dummyjson.com/products?skip=${(page - 1) * 10}&limit=10`)
         .then(response => response.json())
         .then(data => {
+
             let itemsBox = document.getElementById('items');
             data.products.forEach(element => {
 
-                let isAdded = wishlist.some(x => x == element.id);
+                let isAdded = wishlist.some(x=>x==element.id);
 
                 let card = `
                 <div class="col-md-4">
@@ -41,19 +67,36 @@ function LoadinPages() {
                     <h5 class="card-title">${element.title} - ${element.price}</h5>
                     <p class="card-text">${element.description}</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
-                      <i class="fa-${isAdded ? "solid" : "regular"} fa-heart float-end add-to-wishlist" style="cursor: pointer;color:${isAdded ? "red" : "black"}"></i>
+                      <i class="fa-${isAdded?"solid":"regular"} fa-heart float-end add-to-wishlist" style="cursor: pointer;color:${isAdded?"red":"black"}"></i>
                     </div>
                   </div>
                 </div>`
-                itemsBox.innerHTML += card;
+                itemsBox.innerHTML+=card;
             })
-        }).then()
+        }).then(()=>{
+            document.querySelectorAll('.add-to-wishlist').forEach(elem=>{
+                elem.addEventListener('click',function(e){
+                    let id = this.parentNode.parentNode.getAttribute('data-id');
+                   
 
+                    let itemIndex = wishlist.indexOf(id);
+
+                    if(itemIndex==-1){
+                        wishlist.push(id);
+                        this.classList.remove('fa-regular')
+                        this.classList.add('fa-solid')
+                        this.style.color = "red";
+                    }
+                    else{
+                        wishlist.splice(itemIndex,1);
+                        this.classList.remove('fa-solid')
+                        this.classList.add('fa-regular')
+                        this.style.color = "black";
+                    }
+
+                    wishCount.innerText = wishlist.length;
+                    localStorage.setItem('wishlist',JSON.stringify(wishlist));
+                })
+            })
+        })
 }
-
-
-
-
-
-
-
